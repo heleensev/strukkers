fc5e4f90031386542475afefb1b9452ec51f3fff
sevvy <heleenseverin@hotmail.com>
2019-01-05 15:35:26 +0100

    fix the flow up to char equation

--- DS_RecurrenceRelations/recurrence.py
+++ DS_RecurrenceRelations/recurrence.py
@@ -1,11 +1,11 @@
 import fire
 import os
 import sys
-
-sys.path.append('/home/sevvy/Documents/data science/ds/computer assignment/recurrence_relation')
+import re
+sys.path.append('../../recurrence_relation')
 
 from DS_RecurrenceRelations.RecurrenceSolver import raw_parser
-from DS_RecurrenceRelations.RecurrenceSolver import analyzer
+from DS_RecurrenceRelations.RecurrenceSolver import equationbuilder
 from DS_RecurrenceRelations.RecurrenceSolver import solver
 
 
@@ -13,42 +13,37 @@ class Recurrence:
 
     def __init__(self, inputdir):
         self._inputdir = inputdir
-        # self._outputdir = outputdir
 
         self._inputstr = str()
         self._parsed = self._parse_input()
-        self._analyzer = self._analyze()
+        self._equations = self._build_equation()
 
 
 
     def _open_file(self):
         try:
-            with(self._inputdir, 'r') as inputdir:
-                self._inputstr = open(inputdir, 'r')
+            with open(self._inputdir, 'r') as inputdir:
+                return inputdir.read()
 
         except FileNotFoundError:
             print('Input directory is not valid')
             exit(1)
 
     def _parse_input(self):
-        import re
 
-        # remove whitespaces
-        input_str = self._inputstr.replace(' ','')
-        # check standard format
-        eq_pattern = r'eqs\:=\[s\(.*\)=.*,+.*\];'
-        match = re.findall(eq_pattern, input_str, re.S | re.M)
-        if not match:
-            print('warning: input file not of expected form')
+        inputstr = self._open_file()
 
-        parsed = raw_parser.Parser(self._inputstr)
+        parsed = raw_parser.Parser(inputstr)
+
+        print(parsed._recurrence)
+        print(parsed._initials_dict)
         return parsed
 
-    def _analyze(self):
+    def _build_equation(self):
         parsed = self._parsed
-        analyzed = analyzer.Analyzer(parsed._recurrence, parsed._initials_dict)
+        equations = equationbuilder.EquationBuilder(parsed._recurrence, parsed._initials_dict)
 
-        return analyzed
+        return equations
 
     def _solve_recur(self):
         self._closedform = None
@@ -56,12 +51,23 @@ class Recurrence:
 
 
     def _out_file(self):
-        if not (os.path.exists(self._outputdir)):
-            os.makedirs(self._outputdir)
 
-        with open(self._outputdir, 'w') as out:
+        # check and create output directory
+        outputdir = '../../ds_output_files'
+        if not (os.path.exists(outputdir)):
+            os.makedirs(outputdir)
+
+        # build new output path
+        prev_name = os.path.basename(self._inputdir)
+        filename = os.path.join(outputdir, prev_name)
+
+        # write file out to output path
+        with open(filename, 'w') as out:
             out.write(self._closedform)
 
-
+#
 if __name__ == "__main__":
     fire.Fire(Recurrence)
+
+
+# r = Recurrence("../../ds_assignment_files/comass03.txt")
